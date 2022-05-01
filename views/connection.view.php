71210f682115634c68fcd6e408a24317a2bea56b
Will <103755553+Wilka04@users.noreply.github.com>
2022-05-01 21:01:18 +0200

    commit de modification de connection

--- views/connection.view.php
+++ views/connection.view.php
@@ -4,6 +4,39 @@
     <link rel="stylesheet" type="text/css" href="style/connection.css" />
 </head>
 <body>
+<div class="limiter">
+		<div class="container" style="background-color : rgb(195, 166, 253);">
+			<div class="wrap">
+				<form class="login-form">
+					<a href="accueil.php">
+					<img class="img" src="/Users/kandassamywildrick/Desktop/TakeR.png" alt="TakeR">
+					</a>
+					<h2>Connexion</h2>
+					<br></br>
+					<div class="inputwrap">
+						<input class="inputtext" type="text" name="email ou telephone" placeholder="Email ou Téléphone" required>
+					</div>
+					<div>
+                        <p1>
+						Vous n'avez pas encore de compte ? <a href="inscription.php">Créez-en un ici!</a>
+                        </p1>
+					</div>
+                    <br></br>
+                    <div>
+                        <button class="button"style="background-color:rgb(195, 166, 253) ">
+                        Suivant
+                        </button>
+                    </div>
+                    <br></br>
+					<div>
+						<p2>
+                            <img class="img1" src="/Users/kandassamywildrick/Desktop/configuration.png" alt="configuration"> Options de connexion
+                        </p2>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html>
