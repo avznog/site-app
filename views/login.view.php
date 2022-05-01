254ca3e0a4696b0aebdfad1c6326c8a8f96ca6aa
Will <103755553+Wilka04@users.noreply.github.com>
2022-05-01 21:09:04 +0200

    Commit de modification de login

--- views/login.view.php
+++ views/login.view.php
@@ -4,6 +4,51 @@
     <link rel="stylesheet" type="text/css" href="style/login.css" />
 </head>
 <body>
-
+<div class="limiter1">
+		<div class="container1" style="background-color : rgb(195, 166, 253);">
+			<div class="wrap1">
+                    <a href="accueil.php">
+                        <img class="img2" src="/Users/kandassamywildrick/Desktop/TakeR.png" alt="TakeR">
+                    </a>
+                    <div class="headwrap1">
+                        <h1>Créez votre compte Take'R</h1>
+                    </div>
+                    <br></br>
+				<form class="registration">
+                            <div class="inputgroup">
+                                <div>
+                                    <input class="input" type="text" name="prénom" placeholder="Prénom"required>
+                                    <input class="input" type="text" name="nom de famille" placeholder="Nom de Famille" required>
+                                </div>
+                                <div>
+                                    <input class="input2" type="text" name="pays de naissance" placeholder="Pays de naissance" required>
+                                </div>
+                                <div>
+                                    <input class="input2" type="text" name="date de naissance" placeholder="Date de naissance (JJ/MM/AAAA)"required>
+                                </div>
+                                <hr></hr>
+                                <div>
+                                    <input class="input2" type="mail" name="mail" placeholder="xyz@exemple.com" required>
+                                </div>
+                                <div>
+                                    <input class="input2" type="password" name="pass" placeholder="Mot de passe" required>
+                                </div>
+                                <div>
+                                    <input class="input2" type="password" name="confirmpass" placeholder="Confirmez le mot de passe" required>
+                                </div>
+                                <hr></hr>
+                                <div>
+                                    <input class="input2" type="tel" name="phone"  placeholder="Numéro de téléphone">
+                                </div>
+                                <div>
+                                    <button class="button" style="background-color:rgb(195, 166, 253) ">
+                                    Suivant
+                                    </button>
+                                </div>
+                            </div>
+				</form>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
