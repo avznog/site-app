131372a89f1c123b3a4e46372b07e0e97667492c
Makapaka111 <62110820+Makapaka111@users.noreply.github.com>
2022-05-01 22:50:43 +0200

    Update footer.view.php

--- views/footer.view.php
+++ views/footer.view.php
@@ -3,7 +3,6 @@
     <title>Footer</title>
     <link rel="stylesheet" href="style/footer.css" type="test/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-
 </head>
 <body>
     <footer class="footer">
