8331dbf4b4a2b01a557f9281c74704aed7f0339b
Makapaka111 <62110820+Makapaka111@users.noreply.github.com>
2022-05-28 15:01:19 +0200

    Update footer.view.php

--- views/footer.view.php
+++ views/footer.view.php
@@ -1,48 +1,51 @@
 <!DOCTYPE html>
+<html lang="fr">
 <head>
-    <!-- <title>Footer</title> -->
-    <link rel="stylesheet" type="text/css" href="style/footer.css" />
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"> -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
 </head>
 <body>
-    <footer class="footer">
-        <div class="l1">
-            <div class="c1">
-                <p><a class="jiacu" href="">Entreprise</a></p>
-                <p><a href="">Qui sommes-nous ?</a></p>
-                <p><a href="">Contact</a></p>
-            </div>
-            <div class="c2">
-                <p><a class="jiacu" href="">Support client</a></p>
-                <p><a href="">Questions fréquentes</a></p>
-                <p><a href="faq.view.php">FAQ</a></p>
-            </div>
-        </div>
-        
-        <div class="l2">
-            <p>Suivez nous : </p>
-            <a href=""><i class="fa-brands fa-instagram"></i></a>
-            <a href=""><i class="fa-brands fa-linkedin"></i></a>
-            <a href=""><i class="fa-brands fa-youtube"></i></a>
-            <a href=""><i class="fa-brands fa-facebook-f"></i></a>
-            <a href=""><i class="fa-brands fa-twitter"></i></a>
-        </div>
-        <hr style="height:3px;border:none;border-top:5px double rgb(17, 16, 16);" />
-        
 
-        <div class="l3">
-            <div class="left">
-                <p>&copy; 2022 Gaïa Protect incorporation et de ses filiales. Tous droits réservés. Take’R</p>
-            </div>
-
-            <div class="right">
-                <p><a href="">Politique de confidentialité</a>
-                <a href="">Mentions légales</a>
-                <a href="">Cookies</a></p>
-            </div>
-        </div>
-        
-        
-    </footer>
 </body>
+<footer class="footer">
+    <div class="footer-l1">
+        <div class="footer-c1">
+            <p class="jiacu"><a class="jiacu" href="">Entreprise</a></p>
+            <p class="footer-LJ"><a class="footer-lj" href="">Qui sommes-nous ?</a></p>
+            <p class="footer-LJ"><a class="footer-lj" href="">Contact</a></p>
+        </div>
+        <div class="footer-c2">
+            <p class="jiacu"><a class="jiacu" href="">Support client</a></p>
+            <p class="footer-LJ"><a class="footer-lj" href="">Questions fréquentes</a></p>
+            <p class="footer-LJ"><a class="footer-lj" href="faq.html">FAQ</a></p>
+        </div>
+    </div>
+    
+    <div class="footer-l2">
+        <p class="footer-l2-wz">Suivez nous : </p>
+        <a href=""><i class="fa-brands fa-instagram"></i></a>
+        <a href=""><i class="fa-brands fa-linkedin"></i></a>
+        <a href=""><i class="fa-brands fa-youtube"></i></a>
+        <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+        <a href=""><i class="fa-brands fa-twitter"></i></a>
+    </div>
+    <hr style="height:3px;border:none;border-top:5px double rgb(17, 16, 16);" />
+    
+
+    <div class="footer-l3">
+        <div class="l3-left">
+            <p class="l3-left-wz">&copy; 2022 Gaïa Protect incorporation et de ses filiales. Tous droits réservés. Take’R</p>
+        </div>
+
+        <div class="l3-right">
+            <p class="l3-right-LJ"><a class="l3-right-lj" href="">Politique de confidentialité</a>
+            <a class="l3-right-lj" href="">Mentions légales</a>
+            <a class="l3-right-lj" href="">Cookies</a></p>
+        </div>
+    </div>
+</footer>
 </html>
