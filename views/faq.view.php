29bf4e62ee787d67f90622ac7c8c509151cb0e6d
Makapaka111 <62110820+Makapaka111@users.noreply.github.com>
2022-05-28 15:00:54 +0200

    Update faq.view.php

--- views/faq.view.php
+++ views/faq.view.php
@@ -1,47 +1,53 @@
 <!DOCTYPE html>
+<html lang="fr">
 <head>
-    <title>faq</title>
-    <link rel="stylesheet" type="text/css" href="style/faq.css" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ</title>
+    <link rel="stylesheet" href="faq.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    
 </head>
 <body>
     <div class="imgfaq">
-        <img  src="ressources/faq.jfif" alt="">
+        <img  src="./img/faq.jfif" alt="">
     </div>
-    <div class="question">
-        <div class="q">
-            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
+    <div class="faq-question">
+        <div class="faq-q">
+            <h2 class="QUE">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
+            <p class="REP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
         </div>
     </div>
 
-    <div class="question">
-        <div class="q">
-            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
+    <div class="faq-question">
+        <div class="faq-q">
+            <h2 class="QUE">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
+            <p class="REP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
         </div>
     </div>
 
-    <div class="question">
-        <div class="q">
-            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
+    <div class="faq-question">
+        <div class="faq-q">
+            <h2 class="QUE">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
+            <p class="REP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
         </div>
     </div>
 
-    <div class="question">
-        <div class="q">
-            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
+    <div class="faq-question">
+        <div class="faq-q">
+            <h2 class="QUE">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
+            <p class="REP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
         </div>
     </div>
 
-    <div class="question">
-        <div class="q">
-            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
+    <div class="faq-question">
+        <div class="faq-q">
+            <h2 class="QUE">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus?<button class="btn"><span class="down"><i class="fa-solid fa-angle-down"></i></span><span class="up"><i class="fa-solid fa-angle-up"></i></span></button></h2>
+            <p class="REP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt!</p>
         </div>
     </div>
     <script src="faq.js"></script>
+    
 </body>
 </html>
